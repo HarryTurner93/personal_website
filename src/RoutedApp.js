18cc26b280f165ecca58499d92f184f7bd8b868c
Henry Turner <harry.turner@rovco.com>
2020-10-20 11:42:36 +0100

    Switched to hash router.

--- src/RoutedApp.js
+++ src/RoutedApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from './App';
 
 class RoutedApp extends React.Component {
